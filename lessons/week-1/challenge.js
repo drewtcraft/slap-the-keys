32d063096edd4541ae503bc14835533a91e4519d
andrew <drewtcraft@gmail.com>
2021-05-27 07:02:13 -0400

    add challenge

---
+++ lessons/week-1/challenge.js
@@ -0,0 +1,92 @@
+// ---------
+// CHALLENGE
+// ---------
+
+// Instructions:
+// 1. Go to repl.it (you may need to create an account)
+// 2. Click "New Repl"
+// 3. For the language, selece "Node.js" or "JavaScript"
+// 4. Copy-paste this entire document into the REPL
+// 5. Start on the assignment! Click "run" and check the bottom
+// 		of the page to see the output
+
+// ---------
+// Problem 1
+// ---------
+
+// modify the someNum variable
+let someNum = 0;
+if (someNum > 5) {
+	console.log('exercise 1: PASS');
+} else {
+	console.log('exercise 1: FAIL');
+}
+
+
+// ---------
+// Problem 2
+// ---------
+
+// modify the nomNomNom variable using arithmetic operators (+|-|/|*|%) 
+let nomNomNom = 0;
+if (nomNomNom > 999) {
+	console.log('exercise 2: PASS');
+} else {
+	console.log('exercise 2: FAIL');
+}
+
+
+// ---------
+// Problem 3
+// ---------
+
+// modify the moo variable
+let moo;
+if (typeof moo === 'undefined') {
+	console.log('exercise 3: FAIL');
+} else if (typeof moo === 'null') {
+	console.log('exercise 3: PASS');
+} else {
+	console.log('exercise 3: FAIL');
+}
+
+
+// ---------
+// Problem 4
+// ---------
+
+// modify the honk variable -- it should remain a string
+let honk = '';
+if (honk) {
+	console.log('exercise 4: PASS');
+} else {
+	console.log('exercise 4: FAIL');
+}
+
+
+// ---------
+// Problem 5
+// ---------
+
+// modify one or both of the below variables ("a"|"b")
+let a = true;
+let b = false;
+if (a && b) {
+	console.log('exercise 5: PASS');
+} else {
+	console.log('exercise 5: FAIL');
+}
+
+// ---------
+// Problem 6
+// ---------
+
+// modify one or both of the below variables ("a"|"b")
+let a = false;
+let b = false;
+if (a || b) {
+	console.log('exercise 6: PASS');
+} else {
+	console.log('exercise 6: FAIL');
+}
+
